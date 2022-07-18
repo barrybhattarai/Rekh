ff51ab682af0621c29ab6830f7b2a81e33ca6e39
Bhanu <bhanubhattari13@gmail.com>
2022-07-18 08:03:01 +0530

    Drew a simple circle.

--- main.cc
+++ main.cc
@@ -5,14 +5,19 @@
 #include "Vao.h"
 #include "Shader.h"
 #include "program.h"
+#include <vector>
+#include <cmath>
 
 float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 1.0, 0.0, 0.0,
-        0.5f, -0.5f, 0.0f, 0.0, 1.0, 0.0,
-        0.0f, 0.5f, 0.0f, 0.0, 0.0, 1.0
+        -0.5f, 0.5f, 0.0f, 1.0, 0.0, 0.0,
+        -0.5f, -0.5f, 0.0f, 0.0, 1.0, 0.0,
+        0.5f, -0.5f, 0.0f, 0.0, 0.0, 1.0,
+        0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f
 };
 
+
 int main() {
+    const float PI = atanf(1) * 4;
 
     auto window = MainWindow(600, 600, "Kiran");
     window.makeContextCurrent();
@@ -30,17 +35,31 @@ int main() {
     auto vao = Vao();
     vao.bind();
 
+    std::vector<float> verts;
+    float r = 0.5;
+    float steps = 2 * PI / 100;
+    float angle = 0;
+    while(angle < 2* PI){
+        float x = cosf(angle)/2;
+        float y = sinf(angle)/2;
+        verts.push_back(x);
+        verts.push_back(y);
+        verts.push_back(0);
+        angle+= steps;
+    }
+
     auto vbo = Vbo();
-    vbo.addData(sizeof(vertices), vertices);
-    vbo.attribPointer(0, 3, GL_FLOAT, 6 * sizeof(float), 0);
+    vbo.addData(verts.size()* sizeof(float ), verts.data());
+    vbo.attribPointer(0, 3, GL_FLOAT, 3 * sizeof(float), 0);
     vbo.attribPointer(1, 3, GL_FLOAT, 6 * sizeof(float), 3 * sizeof(float));
 
     vao.enableLocation(0);
-    vao.enableLocation(1);
+//    vao.enableLocation(1);
     program.useProgram();
-    window.exec([]() -> void {
+    window.exec([=]() -> void {
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 100);
+
     });
 
 }
--- shaders/vert_shader.glsl
+++ shaders/vert_shader.glsl
@@ -4,6 +4,6 @@ layout (location = 1) in vec3 aColor;
 out vec3 color;
 void main(){
     gl_Position = vec4(aPos, 1.0);
-    color = aColor;
+    color = vec3 (aPos.x*2, aPos.y*2, 0.5);
 }
 
